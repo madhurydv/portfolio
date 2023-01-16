7268958dd947043b9cea9b7b12672f32a1f65d33
mad_fkn_hur <madhury571@gmail.com>
2023-01-16 16:44:17 +0530

    old commit

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,24 +1,24 @@
 import "./FormStyles.css";
 import React from 'react';
-import emailjs from 'emailjs-com';
+// import emailjs from 'emailjs-com';
 
 
 const Form = () => {
-  function sendEmail(e)
-  {
-      e.preventDefault();
-      emailjs.sendForm(
-        "service_6earas8",
-        "template_vbvk557",
-        e.target,
-        "dUS9VAD1rr8NLCG7s"
-      ).then(res=>{
-        console.log(res);
-      }).catch(err=>console.log(err));
-  }
+  // function sendEmail(e)
+  // {
+  //     e.preventDefault();
+  //     emailjs.sendForm(
+  //       "service_6earas8",
+  //       "template_vbvk557",
+  //       e.target,
+  //       "dUS9VAD1rr8NLCG7s"
+  //     ).then(res=>{
+  //       console.log(res);
+  //     }).catch(err=>console.log(err));
+  // }
   return (
     <div className="form">
-          <form>
+          <form action="mailto:madhury571@gmail.com" method="post" enctype="text/plain">
             <label>Your Name</label>
             <input type="text" name="name" autoComplete="off" placeholder="Enter your name" />
             <label>Email</label>
--- src/components/WorkCardData.js
+++ src/components/WorkCardData.js
@@ -13,12 +13,5 @@ const ProjectCardData = [
         view:"https://maymaygenerator.netlify.app/",
         source:"https://github.com/madhurydv/meme-generator"
     },
-    {
-        imgSrc:require('../assets/project3.jpg'),
-        title:"Travel Journal",
-        text:"A React based website that allows you to search for popular places and provide their location using google Maps.",
-        view:"https://search-my-show.netlify.app/",
-        source:"https://github.com/madhurydv/traveljournal"
-    }
 ];
 export default ProjectCardData;
